afbcd33ad51739ec5d6aead69aa58d998706423c
Zakaria Agha <zakaria@ezwi.fi>
2025-08-01 17:54:13 -0500

    Initial commit

---
+++ main.go
@@ -0,0 +1,50 @@
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+func main() {
+	token := os.Getenv("DISCORD_TOKEN")
+	if token == "" {
+		fmt.Println("Error: DISCORD_TOKEN environment variable not set.")
+		return
+	}
+
+	dg, err := discordgo.New("Bot " + token)
+	if err != nil {
+		fmt.Println("error creating Discord session,", err)
+		return
+	}
+
+	dg.AddHandler(messageCreate)
+
+	err = dg.Open()
+	if err != nil {
+		fmt.Println("error opening connection,", err)
+		return
+	}
+
+	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	dg.Close()
+}
+
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
+	if m.Content == "!ping" {
+		s.ChannelMessageSend(m.ChannelID, "Pong!")
+	}
+}
