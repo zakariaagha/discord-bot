f93899c8d431f9ebfd44d65a9ba41b11cec34da6
Zakaria Agha <zakaria@ezwi.fi>
2025-08-01 17:57:23 -0500

    feat: load discord token from .env file

--- main.go
+++ main.go
@@ -8,9 +8,15 @@ import (
 	"syscall"
 
 	"github.com/bwmarrin/discordgo"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error loading .env file")
+		return
+	}
 	token := os.Getenv("DISCORD_TOKEN")
 	if token == "" {
 		fmt.Println("Error: DISCORD_TOKEN environment variable not set.")
